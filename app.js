8d0205f5ebb1085b8c9b8a0f58d29eddad9b73a0
rex2110 <rakshitvipinjain21@gmail.com>
2023-01-29 02:00:57 +0530

    final commit after adding db

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
-
-const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const posts = [];
+const homeStartingContent =
+  "Many people say that life isn't like a bed of roses. I beg to differ. I think that life is quite like a bed of roses. Just like life, a bed of roses looks pretty on the outside, but when you're in it, you find that it is nothing but thorns and pain. I myself have been pricked quite badly.";
+const aboutContent =
+  "Hi, I am Rakshit Jain, learning and developing front end web apps.";
+const contactContent =
+  "Please contact me on rakshitvipinjain21@gmail.com.";
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {
+  useNewUrlParser: true,
+});
+const postSchema = mongoose.Schema({
+  title: String,
+  content: String,
+});
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", (req, res) => {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts,
+  Post.find({}, (err, foundPost) => {
+    res.render("home", {
+      homeStartingContent: homeStartingContent,
+      posts: foundPost,
+    });
   });
 });
 
@@ -39,22 +49,23 @@ app
     res.render("compose");
   })
   .post((req, res) => {
-    const post = {
-      title: req.body.postTitle,
-      content: req.body.postBody,
-    };
-    posts.push(post);
-    res.redirect("/");
+    const title = req.body.postTitle;
+    const content = req.body.postBody;
+    const post = new Post({
+      title: title,
+      content: content,
+    });
+    post.save((err) => {
+      if (!err) res.redirect("/");
+    });
   });
 
-app.get("/posts/:title", (req, res) => {
-  posts.forEach((post) => {
-    if (_.lowerCase(post.title) === _.lowerCase(req.params.title)) {
-      res.render("post",{post: post});
-    }
+app.get("/posts/:id", (req, res) => {
+  Post.findById(req.params.id,(err,foundPost)=>{
+      res.render("post", { post: foundPost });
   });
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server has started successfully");
 });
